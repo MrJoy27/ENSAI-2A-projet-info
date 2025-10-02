e22cb460c08f8a2e46838e0bcaf21a7d5f5a50eb
jchemin <jules.chemin27@gmail.com>
2025-10-02 14:36:03 +0000

    ajout classe joueur work in progress

--- src/business_object/joueur.py
+++ src/business_object/joueur.py
@@ -1,36 +1,48 @@
+from src.business_object.carte.liste_cartes import ListeCartes
+
 class Joueur:
     """
     Classe représentant un Joueur
 
     Attributs
     ----------
-    id_joueur : int
-        identifiant
-    pseudo : str
-        pseudo du joueur
-    mdp : str
-        le mot de passe du joueur
-    age : int
-        age du joueur
-    mail : str
-        mail du joueur
-    fan_pokemon : bool
-        indique si le joueur est un fan de Pokemon
+    nom:str
+    main:liste[Carte]
+    jetons_restants:int
+    id_compte:int
+
     """
 
-    def __init__(self, pseudo, age, mail, mdp=None, fan_pokemon=False, id_joueur=None):
+    def __init__(self, nom, main, jetons_restants, id_compte):
         """Constructeur"""
-        self.id_joueur = id_joueur
-        self.pseudo = pseudo
-        self.mdp = mdp
-        self.age = age
-        self.mail = mail
-        self.fan_pokemon = fan_pokemon
+        if not(isinstance(nom, str)):
+            raise TypeError("nom doit etre de type str")
+        if not(isinstance(main, ListeCartes)):
+            raise TypeError("main doit etre de type ListeCartes")
+        if not(isinstance(jetons_restants, int)):
+            raise TypeError("jetons_restants doit etre de type int")
+        if not(isinstance(jetons_restants, int)):
+            raise TypeError("id_compte doit etre de type int")
+        self.nom= nom
+        self.main=main
+        self.jetons_restants=jetons_restants
+        self.id_compte=id_compte
 
     def __str__(self):
-        """Permet d'afficher les informations du joueur"""
-        return f"Joueur({self.pseudo}, {self.age} ans)"
+        """Permet d'afficher les informations du joueur sans ses cartes qui restent cachés"""
+        return f"{self.nom} avec {self.jetons_restants}"
 
-    def as_list(self) -> list[str]:
-        """Retourne les attributs du joueur dans une liste"""
-        return [self.pseudo, self.age, self.mail, self.fan_pokemon]
+    def voir_main(self):
+        print(self.main)
+
+    def voir_jetons_restants(self):
+        print(self.jetons_restants)
+    
+    def relancer(nb_jetons):
+        pass
+
+    def suivre():
+        pass
+
+    def couche():
+        pass
